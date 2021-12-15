5c32199c3fbadb9b81021e5205bfda82ac95ecd1
Lucin Perez <lhperez3@urbe.edu.ve>
2021-12-14 21:49:11 -0600

    configuracion para digital ocean

--- market/settings/local.py
+++ market/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['137.184.110.91']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
